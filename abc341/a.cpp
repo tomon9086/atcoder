aac95dd374ce09a7a31fba43305a64e291a275de
tomon9086 <tomon9086@gmail.com>
2024-02-24 20:09:03 +0900

    add abc341/a

---
+++ abc341/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  for (int i = 0; i < N; i++)
+  {
+    cout << 1 << 0;
+  }
+  cout << 1 << endl;
+
+  return 0;
+}
