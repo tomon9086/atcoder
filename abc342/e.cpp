3aa36c3519881ba50fae950fe71597cc03023a74
tomon9086 <tomon9086@gmail.com>
2024-02-24 23:12:31 +0900

    wip abc342/e

---
+++ abc342/e.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+#define print_vec(v)                                           \
+  copy(v.begin(), v.end(), ostream_iterator<int>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+  int N, M;
+  cin >> N >> M;
+
+  for (int i = 0; i < M; i++)
+  {
+    int l, d, k, c, A, B;
+  }
+
+  for (int i = 0; i < N - 1; i++)
+  {
+  }
+
+  return 0;
+}
