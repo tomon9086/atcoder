1359425206c8ec6e9f63d564a96970b82ec6370a
tomon9086 <tomon9086@gmail.com>
2024-02-24 21:11:55 +0900

    :bug: abc342/a

--- abc342/a.cpp
+++ abc342/a.cpp
@@ -1,4 +1,13 @@
-#include "compete.hpp"
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+typedef long long ll;
+
+#define print_vec(v)                                           \
+  copy(v.begin(), v.end(), ostream_iterator<int>(cout, ", ")); \
+  cout << endl;
 
 using namespace std;
 
