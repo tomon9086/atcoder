f72c782df22d49dc815abb068e5450f0ff7c5569
tomon9086 <tomon9086@gmail.com>
2024-02-25 15:54:11 +0900

    add macro; rep

--- templates/cpp/helloworld.cpp
+++ templates/cpp/helloworld.cpp
@@ -4,6 +4,8 @@
 #include <iterator>
 #include <vector>
 
+#define rep(i, n) for (u_long i = 0; i < (n); i++)
+
 #define print_vec(v)                                           \
   copy(v.begin(), v.end(), ostream_iterator<int>(cout, ", ")); \
   cout << endl;
