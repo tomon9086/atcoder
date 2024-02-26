0ec66e89696df1b9938668ea1fdb935670bb6304
tomon9086 <tomon9086@gmail.com>
2024-02-26 23:17:20 +0900

    :+1: print_vec type

--- templates/cpp/helloworld.cpp
+++ templates/cpp/helloworld.cpp
@@ -9,8 +9,8 @@ typedef long long ll;
 
 #define rep(i, n) for (ll i = 0; i < (n); i++)
 #define print_pair(pair) cout << "{}" << pair.first << ", " << pair.second << "}" << endl
-#define print_vec(v)                                           \
-  copy(v.begin(), v.end(), ostream_iterator<int>(cout, ", ")); \
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
   cout << endl;
 
 using namespace std;
