7a1d4edafeadc68bfa4316b4c331581df8a6ab70
tomon9086 <tomon9086@gmail.com>
2024-02-27 14:51:56 +0900

    add abs/ABC086A

---
+++ src/abs/ABC086A.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define print_pair(pair) cout << "{}" << pair.first << ", " << pair.second << "}" << endl
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+
+  if (a * b % 2 == 0)
+  {
+    cout << "Even" << endl;
+  }
+  else
+  {
+    cout << "Odd" << endl;
+  }
+
+  return 0;
+}
