9511966395e7b1166a0f91e558675c9304c1bcf9
tomon9086 <tomon9086@gmail.com>
2024-03-01 23:58:12 +0900

    add abc100/b

---
+++ src/abc100/b.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define print_pair(pair) cout << "{}" << pair.first << ", " << pair.second << "}" << endl
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+int main()
+{
+  ll d, n;
+  cin >> d >> n;
+
+  ll seed = 1;
+  rep(i, d) seed *= 100;
+  if (n == 100)
+  {
+    n++;
+  }
+
+  cout << seed * n << endl;
+
+  return 0;
+}
