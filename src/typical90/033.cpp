634448ee508409cab71f39a1f770191b1fdee694
tomon9086 <tomon9086@gmail.com>
2024-03-09 17:35:05 +0900

    add typical90/033

---
+++ src/typical90/033.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+#define rep(i, n) for (ll i = 0; i < (n); i++)
+#define print_pair(pair) cout << "{}" << pair.first << ", " << pair.second << "}" << endl
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+int main()
+{
+  int h, w;
+  cin >> h >> w;
+
+  if (w < 2 || h < 2)
+  {
+    cout << w * h << endl;
+  }
+  else
+  {
+    cout << (h / 2 + h % 2) * (w / 2 + w % 2) << endl;
+  }
+
+  return 0;
+}
