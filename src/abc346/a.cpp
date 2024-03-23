6bec2b92189cb34b7933e96a2f6db9ea39dc6ef5
tomon9086 <tomon9086@gmail.com>
2024-03-23 21:08:56 +0900

    add abc346/a

---
+++ src/abc346/a.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+#define rep(i, begin, end) for (ll i = (begin); i < (end); i++)
+#define print_pair(pair) cout << "{}" << pair.first << ", " << pair.second << "}" << endl
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  int prev = -1;
+  rep(i, 0, n)
+  {
+    if (i == 0)
+    {
+      cin >> prev;
+    }
+    else
+    {
+      int curr;
+      cin >> curr;
+      cout << prev * curr << " ";
+      prev = curr;
+    }
+  }
+
+  cout << endl;
+  return 0;
+}
