2ba0911ee2ce53f59a6e25116bf278d78200206c
tomon9086 <tomon9086@gmail.com>
2024-03-31 00:05:40 +0900

    add abc347/c; WA

---
+++ src/abc347/c.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+#define rep(i, begin, end) for (ll i = (begin); i < (end); i++)
+#define print_pair(pair) cout << "{}" << pair.first << ", " << pair.second << "}" << endl
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+ll distance(ll hou, set<ll> d_mod)
+{
+  auto minmax = minmax_element(d_mod.begin(), d_mod.end());
+  ll d_min = *minmax.first;
+  ll d_max = *minmax.second;
+
+  ll zero_hasamanai = d_max - d_min + 1;
+  ll zero_hasamu = (d_min + 1) + (hou - d_max + 1);
+
+  // cout << "zero_hasamanai: " << zero_hasamanai << endl;
+  // cout << "   zero_hasamu: " << zero_hasamu << endl;
+
+  if (zero_hasamanai - zero_hasamu > 0)
+  {
+    vector<ll> v(d_mod.size());
+    copy(d_mod.begin(), d_mod.end(), back_inserter(v));
+    rep(i, 0, v.size()) v[i] = (hou + v[i] - d_min - 1) % hou;
+    auto minmax = minmax_element(v.begin(), v.end());
+    ll d_min = *minmax.first;
+    ll d_max = *minmax.second;
+
+    // print_vec(ll, v);
+
+    ll zero_hasamanai = d_max - d_min + 1;
+    // cout << "zero_hasamanai: " << zero_hasamanai << endl;
+
+    return zero_hasamanai;
+  }
+  else
+  {
+    return zero_hasamanai;
+  }
+}
+
+int main()
+{
+  ll n, a, b;
+  cin >> n >> a >> b;
+  set<ll> d_mod;
+  rep(i, 0, n)
+  {
+    ll d;
+    cin >> d;
+    d_mod.insert(d % (a + b));
+  }
+
+  // print_vec(ll, d_mod);
+  ll dist = distance(a + b, d_mod);
+  if (dist > a)
+  {
+    cout << "No" << endl;
+  }
+  else
+  {
+    cout << "Yes" << endl;
+  }
+
+  return 0;
+}
