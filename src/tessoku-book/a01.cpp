38ceadb3bac4b16d465cc79e59583a45ccce15d2
tomon9086 <tomon9086@gmail.com>
2025-01-04 18:10:05 +0900

    add tessoku-book/a01

---
+++ src/tessoku-book/a01.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+#define rep(i, begin, end) for (ll i = (begin); i < (end); i++)
+#define print_pair(pair) cout << "{" << pair.first << ", " << pair.second << "}" << endl
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  cout << n * n << endl;
+  return 0;
+}
