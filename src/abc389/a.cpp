c645b20845fdb6d8ea8c4257b2ec6f7975ecd91f
tomon9086 <tomon9086@gmail.com>
2025-01-18 21:01:25 +0900

    add abc389/a

---
+++ src/abc389/a.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+#define rep(i, begin, end) for (ll i = (begin); i < (end); i++)
+#define print_pair(pair) cout << "{" << pair.first << ", " << pair.second << "}" << endl
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+int main()
+{
+  string s;
+  cin >> s;
+
+  int l = s[0] - '0';
+  int r = s[2] - '0';
+
+  cout << l * r << endl;
+  return 0;
+}
