7c583566a46d65eea91b058cfbac84f18b6f539b
tomon9086 <tomon9086@gmail.com>
2025-02-01 21:33:00 +0900

    add abc391/c; tle

---
+++ src/abc391/c.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <set>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+typedef long long ll;
+
+#define rep(i, begin, end) for (ll i = (begin); i < (end); i++)
+#define print_pair(pair) cout << "{" << pair.first << ", " << pair.second << "}" << endl
+#define print_vec(type, v)                                      \
+  copy(v.begin(), v.end(), ostream_iterator<type>(cout, ", ")); \
+  cout << endl;
+
+using namespace std;
+
+int main()
+{
+  int n, q;
+  cin >> n >> q;
+
+  vector<int> pegeon_pos(n);
+  rep(i, 0, n) pegeon_pos[i] = i;
+
+  rep(i, 0, q)
+  {
+    int type;
+    cin >> type;
+
+    switch (type)
+    {
+    case 1:
+    {
+      int a, b;
+      cin >> a >> b;
+      pegeon_pos[a - 1] = b - 1;
+      break;
+    }
+
+    case 2:
+    {
+      vector<int> pegeons_in_nest(n, 0);
+      rep(i, 0, n)
+      {
+        pegeons_in_nest[pegeon_pos[i]]++;
+      }
+
+      int count = 0;
+      rep(i, 0, n)
+      {
+        if (pegeons_in_nest[i] >= 2)
+          count++;
+      }
+
+      // print_vec(int, pegeons_in_nest);
+
+      cout << count << endl;
+      break;
+    }
+    }
+  }
+
+  return 0;
+}
